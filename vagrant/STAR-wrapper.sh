56f609e5db40d7e593c4b8c25abc3f0c86f98c66
Stuart Archer <stuart.k.archer@googlemail.com>
2016-09-06 17:44:50 +1000

    Fix STAR wrapper for cases where --readFilesCommand zcat is 1st arg
    add .gitignore

--- vagrant/STAR-wrapper.sh
+++ vagrant/STAR-wrapper.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-function join_by { local IFS="$1"; shift; echo "$*"; }
-INCMDA=( "$@" )
-REGEX1="(.+)--readFilesCommand zcat(.+)"
+IFS=' '
+REGEX1="(.*)--readFilesCommand zcat(.*)"
 REGEX2="(.+--readFilesIn )([^[:space:]]+)(.+)"
-inCmdStr=`join_by ' ' "${INCMDA[@]}"`
+inCmdStr=$*
 if [[ $inCmdStr =~ $REGEX1 ]];
   then inCmdStr=${BASH_REMATCH[1]}${BASH_REMATCH[2]}
-  echo TRUE1
-  echo $inCmdStr 
   if [[ $inCmdStr =~ $REGEX2 ]];
-    then echo TRUE2
+    then
     inCmdStr=${BASH_REMATCH[1]}' <(zcat '${BASH_REMATCH[2]}') '${BASH_REMATCH[3]};
   fi;
 fi;
-
-/bin/bash -c "STAR.orig $inCmdStr"
+exec /bin/bash -c "STAR.orig $inCmdStr"
 
 
